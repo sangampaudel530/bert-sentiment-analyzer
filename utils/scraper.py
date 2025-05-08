b620f2e44310e726fbde2227a0a0a992c67c327b
poudelsangam77 <sangampaudel530@gmail.com>
2025-05-08 12:55:09 +0545

    deployment streamlit apt

--- utils/scraper.py
+++ utils/scraper.py
@@ -1,5 +1,3 @@
-# utils/scraper.py
-
 import time
 import requests
 import shutil
@@ -11,7 +9,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 OMDB_API_KEY = "ad0e3181"  # Replace this with your actual OMDB API key
 
-
 def get_movie_id(title):
     url = f"http://www.omdbapi.com/?t={title}&apikey={OMDB_API_KEY}"
     response = requests.get(url)
@@ -20,20 +17,19 @@ def get_movie_id(title):
         return data.get("imdbID"), data.get("Poster")
     return None, None
 
-
 def get_reviews(movie_id, max_reviews=20):
     reviews = []
 
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")  # Headless mode
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
     )
 
-    # Set correct binary for deployment
-    chrome_path = shutil.which("google-chrome") or shutil.which("google-chrome-stable")
+    # Set the location of Chrome in deployment
+    chrome_path = shutil.which("chromium-browser") or shutil.which("google-chrome")
     if chrome_path:
         chrome_options.binary_location = chrome_path
 
