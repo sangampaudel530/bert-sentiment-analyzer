2dbed4a72d634ed2418a7a3383199263142c7326
poudelsangam77 <sangampaudel530@gmail.com>
2025-05-11 23:15:52 +0545

    updated shree krishna

--- utils/scraper.py
+++ utils/scraper.py
@@ -1,52 +1,57 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import time
 
-def get_reviews(movie_id, max_reviews=20):
-    # Set up Chrome options
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.binary_location = "/usr/bin/google-chrome"
-
-    # Set up Chrome driver service
+def _init_driver():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
     service = Service("/usr/bin/chromedriver")
+    return webdriver.Chrome(service=service, options=options)
 
-    # Start driver
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+def get_movie_id(movie_name):
+    search_name = movie_name.replace(" ", "+")
+    search_url = f"https://www.imdb.com/find?q={search_name}&s=tt&ttype=ft&ref_=fn_ft"
 
-    # IMDb review page
+    driver = _init_driver()
+    driver.get(search_url)
+    time.sleep(2)
+
+    try:
+        first_result = driver.find_element(By.CSS_SELECTOR, "td.result_text a")
+        href = first_result.get_attribute("href")
+        movie_id = href.split("/")[4]
+        return movie_id
+    except Exception as e:
+        print("Movie not found:", e)
+        return None
+    finally:
+        driver.quit()
+
+def get_reviews(movie_id, max_reviews=20):
     url = f"https://www.imdb.com/title/{movie_id}/reviews"
+
+    driver = _init_driver()
     driver.get(url)
+    time.sleep(3)
 
-    # Scroll and click "Load More" until enough reviews
-    while True:
-        try:
-            load_more = driver.find_element("xpath", '//button[contains(text(), "Load More")]')
-            if load_more:
-                driver.execute_script("arguments[0].click();", load_more)
-                time.sleep(2)
-        except:
-            break
+    reviews = []
 
-        page_source = driver.page_source
-        soup = BeautifulSoup(page_source, "html.parser")
-        reviews = soup.select("div.review-container")
-        if len(reviews) >= max_reviews:
-            break
+    try:
+        review_elements = driver.find_elements(By.CSS_SELECTOR, "div.review-container")
+        for review_element in review_elements[:max_reviews]:
+            try:
+                text = review_element.find_element(By.CSS_SELECTOR, ".text.show-more__control").text
+                reviews.append(text)
+            except:
+                continue
+    except Exception as e:
+        print("Error extracting reviews:", e)
+    finally:
+        driver.quit()
 
-    # Final review extraction
-    reviews = soup.select("div.review-container")
-    extracted = []
-    for r in reviews[:max_reviews]:
-        review_text = r.select_one("div.text.show-more__control")
-        if review_text:
-            extracted.append(review_text.text.strip())
-
-    driver.quit()
-    return extracted
+    return reviews
