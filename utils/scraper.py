e827ddd67af48abf5a984812503b4e6fa36a13a8
poudelsangam77 <sangampaudel530@gmail.com>
2025-05-12 12:39:28 +0545

    final push

--- utils/scraper.py
+++ utils/scraper.py
@@ -7,14 +7,14 @@ import time
 
 def get_reviews(movie_name, max_reviews=20):
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless=new')  # Use modern headless mode
+    # options.add_argument('--headless=new')  # Uncomment for headless mode
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
     driver = webdriver.Chrome(options=options)
 
     try:
-        # Step 1: Go to IMDb and search for the movie
+        # Step 1: Go to IMDb and search the movie
         driver.get("https://www.imdb.com")
         search_box = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, 'suggestion-search'))
@@ -28,22 +28,18 @@ def get_reviews(movie_name, max_reviews=20):
         )
         driver.find_element(By.CSS_SELECTOR, "div.ipc-metadata-list-summary-item__c a").click()
 
-        time.sleep(2)  # Give the movie page time to load
-
-        # Step 3: Navigate to the 'User reviews' tab
+        # Step 3: Click the 'User Reviews' tab
         WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "div.sc-466f296a-0.jLmGLJ li:nth-child(2) a"))
+            EC.presence_of_element_located((By.CSS_SELECTOR, "a[href*='/reviews']"))
         )
-        user_review_link = driver.find_element(By.CSS_SELECTOR, "div.sc-466f296a-0.jLmGLJ li:nth-child(2) a")
-        driver.execute_script("arguments[0].click();", user_review_link)
+        review_tab = driver.find_element(By.CSS_SELECTOR, "a[href*='/reviews']")
+        driver.execute_script("arguments[0].click();", review_tab)
 
-        # Step 4: Wait for the reviews to appear
+        # Step 4: Wait for reviews to load
         WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.ipc-overflowText--listCard div.ipc-html-content"))
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.ipc-overflowText--listCard div > div > div"))
         )
-
-        # Step 5: Collect reviews
-        review_elements = driver.find_elements(By.CSS_SELECTOR, "div.ipc-overflowText--listCard div.ipc-html-content")
+        review_elements = driver.find_elements(By.CSS_SELECTOR, "div.ipc-overflowText--listCard div > div > div")
         reviews = [el.text.strip() for el in review_elements if el.text.strip()]
 
         return reviews[:max_reviews]
@@ -54,3 +50,5 @@ def get_reviews(movie_name, max_reviews=20):
 
     finally:
         driver.quit()
+
+
